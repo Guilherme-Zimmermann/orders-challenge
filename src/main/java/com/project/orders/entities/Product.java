245454ffdc80d6e1929214051032c4c39459d688
GuiWill06 <zimmerma83@gmail.com>
2023-08-28 16:27:15 -0400

    Product entity

---
+++ src/main/java/com/project/orders/entities/Product.java
@@ -0,0 +1,79 @@
+package com.project.orders.entities;
+
+import java.io.Serializable;
+
+public class Product implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String title;
+    private String description;
+    private Double price;
+    
+    public Product(){
+    }
+
+    public Product(Long id, String title, String description, Double price) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Product other = (Product) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
