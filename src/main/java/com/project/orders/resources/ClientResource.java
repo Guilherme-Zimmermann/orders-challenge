d6a4e28ea131499dd19168cf5e5bc29699aad7ce
GuiWill06 <zimmerma83@gmail.com>
2023-08-28 17:13:41 -0400

    Client resource

---
+++ src/main/java/com/project/orders/resources/ClientResource.java
@@ -0,0 +1,33 @@
+package com.project.orders.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.orders.Services.ClientService;
+import com.project.orders.entities.Client;
+
+@RestController
+@RequestMapping
+public class ClientResource {
+    
+    @Autowired
+    private ClientService service;
+
+    @GetMapping
+    public ResponseEntity<List<Client>> findAll(){
+        List<Client> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Client> findById(@PathVariable Long id){
+        Client client = service.findById(id);
+        return ResponseEntity.ok().body(client);
+    }
+}
