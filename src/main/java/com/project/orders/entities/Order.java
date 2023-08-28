e3d73bced993704d845233c9618ba3ef4d3488c8
GuiWill06 <zimmerma83@gmail.com>
2023-08-28 16:13:09 -0400

    Order entity

---
+++ src/main/java/com/project/orders/entities/Order.java
@@ -0,0 +1,60 @@
+package com.project.orders.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class Order implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+    private Long id;
+    private Instant moment;
+
+    public Order(){
+    }
+
+    public Order(Long id, Instant moment) {
+        this.id = id;
+        this.moment = moment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Order other = (Order) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
